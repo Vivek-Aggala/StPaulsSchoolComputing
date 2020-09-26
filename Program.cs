5a313ebdbbbbec94bd42d3bf1eb951d32a40438c
Vivek-Aggala <71449499+Vivek-Aggala@users.noreply.github.com>
2020-09-26 20:27:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace functions_Vivek
+{
+    class Program
+    {
+        
+        static int SumTwo(int a, int b)
+        {
+            return(a + b);
+        }
+        static double SumTwo(double a, double b)
+        {
+            return (a + b);
+
+        }
+        static int Spacey(string e)
+        {
+            var strArray = e.Split(new char[] { ' ' });
+            int countSpace = strArray.Length - 1;
+            return countSpace;
+
+        }
+        public static string ToBinary(int n)
+        {
+            if (n < 2) return n.ToString();
+
+            var divisor = n / 2;
+            var remainder = n % 2;
+
+            return ToBinary(divisor) + remainder;
+
+        }
+        static int AddDigits(int n)
+        {
+             int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO");
+            Console.WriteLine(ToBinary(67));
+            Console.WriteLine(Spacey("Luke, I am your father"));
+            Console.WriteLine(AddDigits(4765));
+        }
+
+
+    }
+}
