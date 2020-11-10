9c48113b22a6a56364c557557bd6676ece8b573c
Vivek-Aggala <71449499+Vivek-Aggala@users.noreply.github.com>
2020-11-10 12:20:50 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+
+namespace reverse_polish_lol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* code works by inputing each element of stack on different lines-Vivek*/
+            Console.WriteLine("Hello World!");
+            var stack = new Stack<double>();
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "+")
+                {
+                    stack.Push(stack.Pop() + stack.Pop());
+                    Console.WriteLine(stack.Peek());
+                }
+                else if (input == "*")
+                {
+                    stack.Push(stack.Pop() * stack.Pop());
+                    Console.WriteLine(stack.Peek());
+                }
+                else
+                {
+                    double result;
+                    if (Double.TryParse(input, out result))
+                    {
+                        stack.Push(result);
+                    }
+                }
+            }
+        }
+        
+
+    }
+}
